64e9a77f51bd3f8f2ee64d28f9fe2ad68d054aa4
eggward <chua.edward@gmail.com>
2015-04-14 22:51:53 +0800

    TradeGecko Coding Exercise

--- app.js
+++ app.js
@@ -1,40 +1,87 @@
 /*
-    # Endpoint URL #
 
-    https://api.github.com/legacy/repos/search/{query}
+Author: Edward Rey Chua
+Email: chua.edward@gmail.com
 
-    Note: Github imposes a rate limit of 60 request per minute. Documentation can be found at http://developer.github.com/v3/.
-
-    # Example Response JSON #
-
-    {
-      "repositories": [
-        {
-          "created_at": "2014-01-13T02:37:26Z",
-          "description": "A Ruby interface to the TradeGecko API.",
-          "followers": 1,
-          "fork": false,
-          "forks": 2,
-          "has_downloads": true,
-          "has_issues": true,
-          "has_wiki": true,
-          "homepage": null,
-          "language": "Ruby",
-          "name": "gecko",
-          "open_issues": 3,
-          "owner": "tradegecko",
-          "private": false,
-          "pushed": "2014-07-29T08:18:51Z",
-          "pushed_at": "2014-07-29T08:18:51Z",
-          "score": 16.24838,
-          "size": 1617,
-          "type": "repo",
-          "url": "https://github.com/tradegecko/gecko",
-          "username": "tradegecko",
-          "watchers": 1
-        }
-      ]
-    }
 */
 
 
+(function ($, _) {
+
+
+    //Github API object    
+    var api = {
+        search: function (str) {
+            return $.get("https://api.github.com/search/repositories", { q: str });
+        }
+    };
+
+    //Templates
+    var templates = {
+        result: '<% _.forEach(items, function(item) { %><li><a href="#" class="details" data-id="<%= item.id%>"><%- item.full_name%> </a><%= itemDetail({item:item}) %></li><% }); %>',
+        resultDetail: '<ul id="detail<%= item.id%>" style="display: none"><li>language: <%= item.language%></li><li>url: <%- item.html_url%></li><li>description: <%- item.description%></li><li>followers: <ul data-url="<%= item.owner.followers_url%>" id="follower<%= item.id%>"></ul></li></ul>',
+        followers: '<% _.forEach(items, function(item) { %><li><%- item.login%></li><% }); %>'
+    };
+
+
+    //Main App
+    var app = {
+
+
+        init: function () {
+
+            var $btn = $('#btnSearch');
+            var $results = $('#results');
+            var $resultContainer = $('#resultList');
+
+            var compiledResult = _.template(templates.result);
+            var compiledDetail = _.template(templates.resultDetail);
+            var compiledFollowers = _.template(templates.followers);
+
+            $btn.on('click', function (e) {
+
+                var searchStr = $('#search').val();
+
+                api.search(searchStr).done(function (data) {
+                    data.itemDetail = compiledDetail;
+
+                    $resultContainer
+                        .empty()
+                        .append(compiledResult(data))
+
+                    console.log(JSON.stringify(data, null, '\t'));
+                });
+            });
+
+
+            $results.on('click', function (e) {
+                e.stopPropagation();
+                var $target = $(e.target);
+
+                if ($target.prop('class') === 'details') {
+
+                    var id = $target.data('id'),
+                    $followerList = $('#follower' + id),
+                    $detail = $('#detail' + id);
+
+                    $detail.toggle();
+
+                    if($detail.is(':visible')){
+                        $.get($followerList.data('url'), {}).done(function (data) {
+                            $followerList.empty()
+                            .append(compiledFollowers({ items: data }));
+                        });
+                    }
+
+                }
+
+            });
+        }
+    };
+
+
+    $('document').ready(function () {
+        app.init();
+    });
+
+})($, _);
